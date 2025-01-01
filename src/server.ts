c56d6b46d67fcd0e3c4fc989bda1f26fa34fd316
Abidur Rahman Arko <aroarko.sd@gmail.com>
2025-01-01 22:34:58 +0800

    prettier applied

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,36 @@
-/* eslint-disable no-unused-vars */
-/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable no-console */
+import { Server } from 'http';
 import mongoose from 'mongoose';
 import app from './app';
 import config from './app/config';
+
+let server: Server;
 
 async function main() {
   try {
     await mongoose.connect(config.database_url as string);
 
-    app.listen(config.port, () => {
-      console.log(`Example app listening on port ${config.port}`);
+    server = app.listen(config.port, () => {
+      console.log(`app is listening on port ${config.port}`);
     });
-  } catch (error) {
-    // console.log(error);
+  } catch (err) {
+    console.log(err);
   }
 }
 
 main();
+
+process.on('unhandledRejection', () => {
+  console.log(`😈 UnahandledRejection is detected , shutting down ...`);
+  if (server) {
+    server.close(() => {
+      process.exit(1);
+    });
+  }
+  process.exit(1);
+});
+
+process.on('uncaughtException', () => {
+  console.log(`😈 UncaughtException is detected , shutting down ...`);
+  process.exit(1);
+});
