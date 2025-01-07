dc094cad05c8d24c0aa08f5025592ea896ff3954
Abidur Rahman Arko <aroarko.sd@gmail.com>
2025-01-07 17:15:53 +0800

    auth bearer token updated

--- src/app/middlewares/auth.ts
+++ src/app/middlewares/auth.ts
@@ -1,3 +1,5 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import config from '../config';
 import AppError from '../errors/AppErrror';
 import { TUserRole } from '../modules/User/user.interface';
@@ -10,39 +12,46 @@ import User from '../modules/User/user.model';
 const auth = (...requiredRoles: TUserRole[]) => {
   return catchAsync(async (req, res, next) => {
     // Retrieve token from headers
-    const token = req.headers.authorization;
+    const authHeader = req.headers.authorization;
 
-    if (!token) {
+    if (!authHeader || !authHeader.startsWith('Bearer ')) {
       throw new AppError(httpStatus.UNAUTHORIZED, 'You are not authorized!');
     }
 
-    // Verify the token and decode it
-    const decoded = jwt.verify(
-      token,
-      config.jwt_access_secret as string,
-    ) as JwtPayload;
-    const { userEmail, userRole } = decoded;
+    // Extract the token after 'Bearer '
+    const token = authHeader.split(' ')[1];
 
-    // Check if user exists
-    const user = await User.findOne({ email: userEmail });
-    if (!user) {
-      throw new AppError(httpStatus.NOT_FOUND, 'This user is not found!');
+    try {
+      // Verify the token and decode it
+      const decoded = jwt.verify(
+        token,
+        config.jwt_access_secret as string,
+      ) as JwtPayload;
+      const { userEmail, userRole } = decoded;
+
+      // Check if user exists
+      const user = await User.findOne({ email: userEmail });
+      if (!user) {
+        throw new AppError(httpStatus.NOT_FOUND, 'This user is not found!');
+      }
+
+      // Check if user is blocked
+      if (user.isBlocked) {
+        throw new AppError(httpStatus.FORBIDDEN, 'This user is blocked');
+      }
+
+      // Check if the user's role is authorized
+      if (requiredRoles.length && !requiredRoles.includes(userRole)) {
+        throw new AppError(httpStatus.UNAUTHORIZED, 'You are not authorized!');
+      }
+
+      // Attach user information to the request object
+      req.user = decoded;
+
+      next();
+    } catch (error) {
+      throw new AppError(httpStatus.UNAUTHORIZED, 'Invalid or expired token!');
     }
-
-    // Check if user is blocked
-    if (user.isBlocked) {
-      throw new AppError(httpStatus.FORBIDDEN, 'This user is blocked');
-    }
-
-    // Check if the user's role is authorized
-    if (requiredRoles && !requiredRoles.includes(userRole)) {
-      throw new AppError(httpStatus.UNAUTHORIZED, 'You are not authorized!');
-    }
-
-    // Attach user information to the request object
-    req.user = decoded;
-
-    next();
   });
 };
 
